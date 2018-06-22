252dfa31233a3f9a32b0ffb734634fc2dd0d1a5b
Manuel Betancurt <manuel.betancurt@smokeball.com>
2018-06-22 10:17:06 +1000

    update to use path to file with out private, check if file exists

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -12,7 +12,7 @@ NSExtensionContext* extensionContext;
     NSString* type;
     NSString* value;
     NSString* origin;
-
+    
 }
 
 - (UIView*) shareView {
@@ -24,17 +24,19 @@ RCT_EXPORT_MODULE();
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     //object variable for extension doesn't work for react-native. It must be assign to gloabl
     //variable extensionContext. in this way, both exported method can touch extensionContext
     extensionContext = self.extensionContext;
-
+    
     UIView *rootView = [self shareView];
     if (rootView.backgroundColor == nil) {
         rootView.backgroundColor = [[UIColor alloc] initWithRed:1 green:1 blue:1 alpha:0.1];
     }
-
-
+    
+    //find files on path
+    //    [self listAllFiles];
+    
     self.view = rootView;
 }
 
@@ -56,6 +58,9 @@ RCT_REMAP_METHOD(data,
             if(err) {
                 reject(@"error", err.description, nil);
             } else {
+                NSLog(@"tha contentType:%@", contentType);
+                NSLog(@"tha val:%@", val);
+                
                 resolve(@{
                           @"type": contentType,
                           @"value": val,
@@ -74,12 +79,16 @@ RCT_REMAP_METHOD(data,
 
 - (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
     @try {
+        
+        NSLog(@"inputs: %ld", context.inputItems.count);
+        NSLog(@"input items:: %@", context.inputItems);
+        
         NSExtensionItem *item = [context.inputItems firstObject];
         NSArray *attachments = item.attachments;
         __block NSItemProvider *objProvider = nil;
         __block NSString *objIdentifier = nil;
-
-
+        
+        
         [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
             objProvider = provider;
             if ([provider hasItemConformingToTypeIdentifier:ITEM_IDENTIFIER]) {
@@ -90,11 +99,33 @@ RCT_REMAP_METHOD(data,
                 *stop = YES;
             }
         }];
-
+        
         if (objProvider != nil && objIdentifier != nil) {
             [objProvider loadItemForTypeIdentifier:objIdentifier options:nil completionHandler:^(id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
                 NSURL *url = (NSURL *)item;
-                callback([url absoluteString], objIdentifier, nil);
+                
+                // check file exists!
+                
+                //                NSLog(@"URLByResolvingSymlinksInPath :%@", [url URLByResolvingSymlinksInPath].absoluteString);
+                //                NSLog(@"URLByResolvingSymlinksInPath :%@", [url URLByResolvingSymlinksInPath]);
+                //                NSLog(@"absoluteString :%@", url.absoluteString);
+                
+                
+                
+                //                if ([[NSFileManager defaultManager] fileExistsAtPath:[url path]]){
+                //
+                //                    long long fileSize = [[[NSFileManager defaultManager] attributesOfItemAtPath:[url path] error:nil][NSFileSize] longLongValue];
+                //                    NSLog(@"exists with size: %lld", fileSize);
+                //
+                //                    NSData* data = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&error];
+                //
+                //                    NSLog(@"file data:: \n%@", data);
+                //
+                //                    }else{
+                //                        NSLog(@" file doesnt exist");
+                //                    }
+                
+                callback([[url URLByResolvingSymlinksInPath] absoluteString], objIdentifier, nil);
             }];
         } else {
             callback(@"", @"", nil);
@@ -106,5 +137,26 @@ RCT_REMAP_METHOD(data,
         }
     }
 }
+
+
+
+
+//- (void)listAllFiles {
+//
+//    //----- LIST ALL FILES -----
+//    NSLog(@"LISTING ALL FILES FOUND");
+//
+//    int Count;
+//
+//    NSArray *paths = NSSearchPathForDirectoriesInDomains
+//    (NSDocumentDirectory, NSUserDomainMask, YES);
+//    NSString *documentsDirectory = [paths objectAtIndex:0];
+//
+//    NSArray *directoryContent = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:documentsDirectory error:NULL];
+//    for (Count = 0; Count < (int)[directoryContent count]; Count++)
+//    {
+//        NSLog(@"File %d: %@", (Count + 1), [directoryContent objectAtIndex:Count]);
+//    }
+//}
 
 @end
